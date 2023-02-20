65653bb4b2a0f7e80107ee581018c6eb600d815e
Ghayoor <ghayoorhussain701@gmail.com>
2023-02-20 15:26:57 +0500

    Null aware operators

--- dart_lang/bin/optionals/example2.dart
+++ dart_lang/bin/optionals/example2.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
   String? name;
   String? fullName;
-  final foo = name ?? fullName;
+  final foo = name ?? fullName ?? 'Ghayoor Hussain';
   print(foo.runtimeType);
 }
