594283842f77cffb2683abae2fe756d2cb7f2351
Ghayoor <ghayoorhussain701@gmail.com>
2023-02-26 04:30:00 +0500

    Nullable type Promotion

---
+++ dart_lang/bin/optionals/nullable_type_promotion.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  final String? fName = null;
+
+  if (fName == null) {
+    print('FName is Null');
+  } else {
+    final int length = fName.length;
+    print(length);
+  }
+}
