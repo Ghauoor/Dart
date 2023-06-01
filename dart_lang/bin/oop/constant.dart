af3270d0d77bc83e7da8a51f512c871234ade934
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-06-01 10:31:59 +0500

    static keyword

---
+++ dart_lang/bin/oop/constant.dart
@@ -0,0 +1,8 @@
+class Constant {
+  static String greeting = 'Hello, how are you';
+  static String bye = 'Bye';
+
+  static int giveMeSomeValue() {
+    return 10;
+  }
+}
--- dart_lang/bin/oop/main.dart
+++ dart_lang/bin/oop/main.dart
@@ -1,11 +1,13 @@
-import 'cookie.dart';
+import 'constant.dart';
 
 void main(List<String> args) {
-  final cookie = Cookie(shap: 'React', size: 12);
+  //final cookie = Cookie(shap: 'React', size: 12);
   // cookie.baking();
   // final cookieCooling = cookie.isCooling();
   // print(cookieCooling);
 
   // cookie.shap = 'Rectangele';
   // cookie.size = 12.3;
+
+  print(Constant.greeting);
 }
